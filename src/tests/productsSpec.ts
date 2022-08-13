7b52be87a308d98eaebbf718d52a2a7b617e0122
AhmedZein <ahmedzain503@gmail.com>
2022-08-13 17:28:10 +0200

    adding test cases to product endpoint

--- src/tests/productsSpec.ts
+++ src/tests/productsSpec.ts
@@ -1,25 +1,72 @@
 import supertest from "supertest";
 import app from '../server';
-
+import {productsConnectionDB , Product} from '../modules/products';
+import client from "../database";
+import { PoolClient , QueryResult } from "pg";
 const response = supertest(app);
+const store = new productsConnectionDB() ;
 
-describe('Products end-point', () => {
-
-    it('Index functionality gets all products 200' , async () => {
-        const responseObject = await response.get('/products');
-        expect( responseObject.status).toEqual(200);
+describe('Products End-Point', () => {
+   
+    describe('Products HTTP 1.1 status codes', () => {
+        
+        it('Index function  { gets all products 200 }' , async () => {
+            const responseObject = await response.get('/products');
+            expect( responseObject.status).toEqual(200);
+        });
+        it('Show function { gets a specific product 200 }' , async () => {
+            const responseObject = await response.get('/products/1');
+            expect( responseObject.status).toEqual(200);
+        });
+        it('Create function without a token or login info 400`' , async () => {
+            const responseObject = await response.post('/products');
+            expect( responseObject.status).toEqual(400);
+        });
     });
-    it('Show functionality get a specific product 200' , async () => {
-        const responseObject = await response.get('/products/1');
-        expect( responseObject.status).toEqual(200);
+    describe('Products Module', () => {
+       
+        describe('Products Module The declartion ', () => {  
+            it ('check if the index function is declared', async()=>{
+                const responseObject = await store.index() ;
+                expect ( responseObject ).toBeDefined();
+            });
+            it ('check if the show function is declared', async()=>{
+                const responseObject = await store.show('1') ;
+                expect ( responseObject ).toBeDefined();
+            });
+        });
     });
+    describe('Products Module { Database integration process }  ', () => {  
+        let conn : PoolClient ;
+        let  allProduct : QueryResult ;
+        beforeAll( async () =>{
+            conn = await client.connect() ;
+        });
+        it ('check the return of the index function', async()=>{
+            try { 
+                const sql = 'SELECT * FROM products';
+                allProduct = await client.query (sql);
+            }catch(error){
+                console.log (error);
+            }
+            const responseObject = await store.index() ;
+            expect ( responseObject ).toEqual(allProduct.rows as Product[]);
+        });
+        it ('check the return of the show function', async()=>{
+            try { 
+                const sql = 'SELECT * FROM products WHERE id = $1';
+                allProduct = await client.query (sql, [ '1' ] );
+            }catch(error){
+                console.log (error);
+            }
+            const responseObject = await store.show('1') ;
+            expect ( responseObject ).toEqual(allProduct.rows[0] as Product);
+        });
+        afterAll (async () => {
+            conn.release();
+        })
+    });   
 });
 
-describe('Users end-point', () => {
 
-    it('Create a User functionality 200' , async () => {
-        const responseObject = await response.post('/users').send({firstName : 'Mohamed' , lastName : 'Zein' , password : "01120273779"});
-        expect( responseObject.status).toEqual(200);
-    });
-});
 
