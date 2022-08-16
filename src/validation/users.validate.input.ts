ae4f95f7b2152d628708018062c7b53fb11eb6f6
AhmedZein <ahmedzain503@gmail.com>
2022-08-16 13:47:52 +0200

    fixing the user create route error

--- src/validation/users.validate.input.ts
+++ src/validation/users.validate.input.ts
@@ -5,17 +5,18 @@ export default async function firstNameAndlastNameAndPasswordValidation (req : R
     const token = authorizationHeader?.split(' ')[1] ;
     if ( token ) {
         next();
+        return;
     }
     if ( !req.body.firstName ) {
-        res.status(400).send('Bad request firstName is missing'); 
+        res.status(400).send('Bad request firstName is missing').end(); 
         return;
     }
     if ( !req.body.lastName )  {
-        res.status(400).send('Bad request lastName is missing'); 
+        res.status(400).send('Bad request lastName is missing').end(); 
         return;
     }
     if ( !req.body.password )  {
-        res.status(400).send('Bad request password is missing'); 
+        res.status(400).send('Bad request password is missing').end(); 
         return;
     }
     next();
